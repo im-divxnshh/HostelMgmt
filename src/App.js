87ffa6181e90ec89a084808baeeccbfdd1f24ee2
SimranKaur <simranmalhik@gmail.com>
2024-12-09 11:49:24 +0530

    fixed the routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import required components
 import UserDashboard from './pages/user/userDb';
-import UserPage from "./pages/user/userPage";
+import UserLogin from "./pages/user/userPage";
 import AdminDashboard from './pages/admin/adminDashboard';
-import AddStudent from './components/adminComps/addStudent';
-import ManageRoom from './components/adminComps/manageRoom';
-import Fees from './components/adminComps/fees';
-import FeesManagement from './components/adminComps/fees';
+
+
+
 import AdminLogin from './pages/admin/adminLogin';
 function App() {
   return (
@@ -14,13 +13,10 @@ function App() {
       <div className="App">
         {/* Set up routes using Routes */}
         <Routes>
-          <Route path="/" element={<AdminLogin/>} /> {/* Route for UserPage at root */}
-          <Route path="/dashboard" element={<UserDashboard />} /> {/* Route for UserDashboard */}
-          <Route path= "/admin-dashboard" element={<AdminDashboard/>} /> 
-          <Route path= "/add-student" element={<AddStudent/>} /> 
-
-          <Route path= "/manage-room" element={<ManageRoom/>} /> 
-          <Route path="/fees" element= {<FeesManagement/>}/>
+          <Route path="/admin-login" element={<AdminLogin/>} /> {/* Route for UserPage at root */}
+          <Route path="/user-login" element={<UserLogin/>} />
+          <Route path="/user-dashboard" element={<UserDashboard />} /> {/* Route for UserDashboard */}
+          <Route path= "/admin-dashboard" element={<AdminDashboard/>} />
           
    
 
